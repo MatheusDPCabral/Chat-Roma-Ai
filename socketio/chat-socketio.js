059a762df5ed98d5f2b4f894aaa5cc445b831938
Carlos Camilo <60012069+carlosscamilo@users.noreply.github.com>
2024-10-18 14:25:26 -0300

    codigo funcional, genesis (#1)
    Co-authored-by: MatheusDPCabral <matheusdelpinocabral@gmail.com>

---
+++ socketio/chat-socketio.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const fs = require("fs");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.json());
+
+io.on("connection", (socket) => {
+    console.log("Usuário conectado " + socket.id);
+
+    socket.on("message", (msg) => {
+        console.log(msg);
+        io.emit("message", msg);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/chat-login.html");
+});
+
+app.get("/chat", (req, res) => {
+    let username = req.query.user || "user-chat";
+    let chat = fs.readFileSync("./socketio/chat-socketio.html", "utf8");
+    chat = chat.replace("user-chat", username);
+    res.end(chat);
+});
+
+server.listen(4001, () => {
+    console.log("Servidor Socket.IO rodando na porta 4001");
+});
