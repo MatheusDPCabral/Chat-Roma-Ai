58b67cc699fe93eb6548e74dbd6681e9fdc9a033
Matheus Cabral <118760691+MatheusDPCabral@users.noreply.github.com>
2024-10-19 05:27:18 +0000

    Melhorias na  estrutura de mensagens e adição do botão modo noturno

--- socketio/chat-socketio.js
+++ socketio/chat-socketio.js
@@ -3,45 +3,39 @@ const http = require("http");
 const socketIo = require("socket.io");
 const fs = require("fs");
 const path = require("path");
-const cors = require("cors"); // Adicionando o CORS
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 
-// Configurando o CORS
 app.use(cors({
-    origin: '*', // Permitindo todas as origens para teste
+    origin: '*',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
 
-// Configuração do Socket.IO com CORS
 const io = socketIo(server, {
     cors: {
-        origin: '*', // Permitindo todas as origens para teste
+        origin: '*',
         methods: ["GET", "POST"]
     }
 });
 
-// Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, '..'))); // Corrigido para usar path.join
+app.use(express.static(path.join(__dirname, '..')));
 
-// Definindo o Socket.IO
 io.on("connection", (socket) => {
     console.log("Usuário conectado " + socket.id);
 
     socket.on("message", (msg) => {
         console.log(`Mensagem de ${msg.username}: ${msg.message}`);
-        io.emit("message", msg); // Envia a mensagem para todos, incluindo o nome de quem enviou
+        io.emit("message", msg);
     });
 });
 
-// Rota para página de login
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "chat-login.html")); // Corrigido para usar path.join
+    res.sendFile(path.join(__dirname, "chat-login.html"));
 });
 
-// Rota para o chat com substituição do nome de usuário
 app.get("/chat", (req, res) => {
     let username = req.query.user || "user-chat";
     let chat = fs.readFileSync(path.join(__dirname, "chat-socketio.html"), "utf8");
@@ -49,7 +43,6 @@ app.get("/chat", (req, res) => {
     res.end(chat);
 });
 
-// Servidor rodando na porta 4001
 server.listen(4001, () => {
     console.log("Servidor Socket.IO rodando na porta 4001");
 });
